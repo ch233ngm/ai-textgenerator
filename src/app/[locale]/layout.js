2e165ded5fd830b38fa74ea0ceb8fbf925d2ee0a
ch233ngm <ch233ngm@gmail.com>
2024-12-15 23:02:48 +0800

    favicons dir

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -11,7 +11,24 @@ export default async function RootLayout({ children, params }) {
   const messages = await getMessages();
   return (
     <ViewTransitions>
-
+      <link
+        rel="apple-touch-icon"
+        sizes="76x76"
+        href="/static/favicons/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/static/favicons/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/static/favicons/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/static/favicons/site.webmanifest"/>
       <html lang={locale} data-theme="light">
         <body>
           <header>
